d97e4fb6c8809adb4cde209eaa67a413990263ed
SUNDUS-THE-CODER <sundusahmed648@gmail.com>
2023-03-08 17:27:37 +0000

    fetch transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [filter, setFilter] = useState(null);
+  const transactionsToDisplay = filter ? transactions.filter((item) => {
+    return item.id === parseInt(filter) || item.description.includes(filter);
+  }) : transactions;
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        let fetchedItems = await fetch("http://localhost:8001/transactions");
+        fetchedItems = await fetchedItems.json();
+        console.log(fetchedItems);
+        setTransactions(fetchedItems);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchItems();
+  }, []);
   return (
     <div>
-      <Search />
+      <Search setFilter={setFilter} />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transactionsToDisplay} />
     </div>
   );
 }
